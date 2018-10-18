54e0ff6a8f366cea204e2349776ed5bfbf66b2be
Lukas Troyer <36337818+lukas-tr@users.noreply.github.com>
2018-10-18 14:53:16 +0200

    readable solution

--- convert.js
+++ convert.js
@@ -1,1 +1,10 @@
-console.log(isNaN({m:1,cm:.01,mm:.001}[process.argv[3]]*process.argv[2]/{m:1,cm:.01,mm:.001}[process.argv[5]]||{kg:1,g:.001}[process.argv[3]]*process.argv[2]/{kg:1,g:.001}[process.argv[5]])||"to"!=process.argv[4]||6!=process.argv.length?"Invalid parameters":`${process.argv[2]} ${process.argv[3]} are ${{m:1,cm:.01,mm:.001}[process.argv[3]]*process.argv[2]/{m:1,cm:.01,mm:.001}[process.argv[5]]||{kg:1,g:.001}[process.argv[3]]*process.argv[2]/{kg:1,g:.001}[process.argv[5]]} ${process.argv[5]}`);
+const length = { m: 1, cm: 0.01, mm: 0.001 };
+const weight = { kg: 1, g: 0.001 };
+const solution =
+  (length[process.argv[3]] * process.argv[2]) / length[process.argv[5]] ||
+  (weight[process.argv[3]] * process.argv[2]) / weight[process.argv[5]];
+console.log(
+  isNaN(solution) || "to" != process.argv[4] || 6 != process.argv.length
+    ? "Invalid parameters"
+    : `${process.argv[2]} ${process.argv[3]} are ${solution} ${process.argv[5]}`
+);
